3b64e1ff4a6a367463530123a6419c0f5d05004d
ysknsid25 <kengo071225@gmail.com>
2023-11-30 10:00:50 +0900

    しずかなインターネット
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -8,6 +8,8 @@ import insta from '../assets/sns/insta.png'
 import note from '../assets/sns/note.png'
 import zenn from '../assets/sns/zenn.png'
 import qiita from '../assets/sns/qiita.png'
+import threads from '../assets/sns/threads.png'
+import shizunet from '../assets/sns/shizunet.png'
 
 type SNS = {
     url: string,
@@ -21,8 +23,10 @@ const snsList: SNS[] = [
     { url: "https://bsky.app/profile/blessingsoftware.bsky.social", icon: bluesky },
     { url: "https://twitter.com/samurai_se", icon: twitter },
     { url: "https://www.instagram.com/samurai_se99/", icon: insta },
+    { url: "https://www.threads.net/@samurai_se99", icon: threads },
     { url: "https://www.amazon.co.jp/hz/wishlist/ls/3FRHPBCKN1Z1L?ref_=wl_share", icon: amazon },
     { url: "https://note.com/samurai_se/", icon: note },
+    { url: "https://sizu.me/ysknsid25", icon: shizunet },
 ]
 
 export const Social = () => {
@@ -31,7 +35,7 @@ export const Social = () => {
             <div className="flex justify-center items-center gap-4 mb-4">
                 <h1 className="text-center text-3xl font-extrabold text-gray-600 underline">SNS</h1>
             </div>
-            <div className="grid grid-cols-4 md:grid-cols-8 gap-4 items-center justify-center mb-8">
+            <div className="grid grid-cols-4 md:grid-cols-6 gap-4 items-center justify-center mb-8">
                 {snsList.map((sns, index) =>
                     <div key={index} className="grid grid-cols-1 gap-4 m-auto">
                         <div className="flex justify-center items-center md:h-16 md:w-16 h-12 w-12 rounded-xl bg-white border-solid border-2 border-gray-200">

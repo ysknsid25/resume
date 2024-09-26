5748bd12724ec1a62f7b1b4a1d433501bad66b10
ysknsid25 <kengo071225@gmail.com>
2024-09-26 20:43:04 +0900

    Resume
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/components/SideJob.tsx
+++ src/components/SideJob.tsx
@@ -4,11 +4,11 @@ const contents = [
     {
         treeType: "🏗",
         title: "株式会社hitocolor様でe-ラーニングシステムの開発",
-        year: "2024 - 現在",
+        year: "2024",
         img: "hitocolor",
         url: "https://www.hitocolor.co.jp/",
         content:
-            "Next.js+LaravelでのWebアプリケーション開発を行なっています。機能開発だけでなく既存テストコードのカバレッジ改善、DXや脆弱性など非機能面での改善のお手伝いもしています。またNest.js×LangChainを利用したマイクロサービスの全体的な技術選定・構築・実装までも担当しています。",
+            "Next.js+LaravelでのWebアプリケーション開発を行なっていました。機能開発だけでなく既存テストコードのカバレッジ改善、DXや脆弱性など非機能面での改善のお手伝いもしていました。またNest.js×LangChainを利用したマイクロサービスの全体的な技術選定・構築・実装までも担当し、Google Cloud にBuildpack,CloudRunを利用してCI/CDパイプラインの構築も行いました。",
     },
 ];
 
@@ -16,8 +16,11 @@ export const SideJob = () => {
     const conditions = (isReceiving: boolean) => {
         return isReceiving ? (
             <ul className="list-disc text-sm font-normal text-gray-400">
-                <li>週20時間程度</li>
-                <li>Kotlin(Ktor) or PHP(Laravel) or React(Next.js)案件</li>
+                <li>月24時間程度</li>
+                <li>
+                    Kotlin(ServerSide Kotlin) or TypeScript (フロントバック) or
+                    GCP案件
+                </li>
                 <li>
                     本業の業務領域に関連しない事業内容であること(アニメ・漫画などに関する商業商品販売・同人誌販売に関係しない事業内容)
                 </li>
@@ -52,7 +55,7 @@ export const SideJob = () => {
                         <h2 className="text-xl font-extrabold text-gray-600 mb-2">
                             条件
                         </h2>
-                        {conditions(false)}
+                        {conditions(true)}
                     </div>
                     <div className="px-8 mt-4"></div>
                 </div>
@@ -66,8 +69,9 @@ export const SideJob = () => {
                         <ul className="list-disc text-sm font-normal text-gray-400">
                             <li>
                                 Webシステム開発。特にPHP(Laravel) or
-                                Kotlin(Ktor) or React(Next.js)案件
+                                Kotlin(Ktor) or TypeScript(Next.js, Nest.js)案件
                             </li>
+                            <li>テストコードの導入支援</li>
                             <li>
                                 ソフトウェアエンジニア同士の交流のためのコミュニティ運営
                             </li>

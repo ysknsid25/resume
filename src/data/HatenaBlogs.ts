e8f5d2cb55a49df2d94c1895cd5710949a1b7965
ysknsid25 <kengo071225@gmail.com>
2024-11-19 14:30:28 +0900

    ラボブログの登録
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/data/HatenaBlogs.ts
+++ src/data/HatenaBlogs.ts
@@ -2,6 +2,11 @@
 export const HatenaBlogs = [
     {
         year: "2024",
+        title: "Google Cloud様主催のハンズオンセミナーに参加してPlatform Engineeringの解像度が128倍上がった話",
+        url: "https://toranoana-lab.hatenablog.com/entry/2024/11/19/120000",
+    },
+    {
+        year: "2024",
         title: "日々の進歩をふりかえるために『今日の学びブログ』を続けている話",
         url: "https://toranoana-lab.hatenablog.com/entry/2024/07/17/120000",
     },

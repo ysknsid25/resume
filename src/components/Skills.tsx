254d38f2c6936e57445f16340f67e78932bcb7fe
ysknsid25 <kengo071225@gmail.com>
2024-11-25 18:20:00 +0900

    Update Skills
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,24 +1,16 @@
 import clsx from "clsx";
 import php from "../assets/program/php.png";
-import docker from "../assets/program/docker.png";
 import gcp from "../assets/program/gcp.png";
-import java from "../assets/program/java.png";
-import js from "../assets/program/js.png";
 import kotlin from "../assets/program/kotlin.png";
 import ktor from "../assets/program/ktor.png";
 import laravel from "../assets/program/laravel.png";
 import linux from "../assets/program/linux.png";
-import mysql from "../assets/program/mysql.png";
 import nextjs from "../assets/program/nextjs.png";
-import oracle from "../assets/program/oracle.png";
 import react from "../assets/program/react.png";
-import spring from "../assets/program/spring.png";
 import ts from "../assets/program/ts.png";
 import nginx from "../assets/program/nginx.png";
-import git from "../assets/program/git.png";
-import github from "../assets/sns/github.png";
-import aws from "../assets/program/aws.png";
 import nest from "../assets/program/nest.png";
+import python from "../assets/program/python.png";
 
 type Skill = {
     name: string;
@@ -27,26 +19,18 @@ type Skill = {
 };
 
 const skills: Skill[] = [
-    { name: "Java", level: 2, icon: java },
-    { name: "Spring", level: 1, icon: spring },
-    { name: "Kotlin", level: 2, icon: kotlin },
-    { name: "Ktor", level: 2, icon: ktor },
-    { name: "js", level: 2, icon: js },
-    { name: "ts", level: 2, icon: ts },
+    { name: "GCP", level: 3, icon: gcp },
+    { name: "Python", level: 2, icon: python },
+    { name: "ts", level: 3, icon: ts },
     { name: "React", level: 2, icon: react },
     { name: "Next.js", level: 2, icon: nextjs },
     { name: "Nest.js", level: 2, icon: nest },
-    { name: "PHP", level: 2, icon: php },
-    { name: "Laravel", level: 2, icon: laravel },
-    { name: "MySQL", level: 2, icon: mysql },
-    { name: "Oracle", level: 2, icon: oracle },
+    { name: "Kotlin", level: 2, icon: kotlin },
+    { name: "Ktor", level: 2, icon: ktor },
+    { name: "PHP", level: 3, icon: php },
+    { name: "Laravel", level: 3, icon: laravel },
     { name: "Nginx", level: 2, icon: nginx },
-    { name: "Docker", level: 2, icon: docker },
-    { name: "Git", level: 2, icon: git },
-    { name: "GitHub", level: 2, icon: github },
     { name: "Linux", level: 2, icon: linux },
-    { name: "GCP", level: 2, icon: gcp },
-    { name: "AWS", level: 1, icon: aws },
 ];
 
 const getLevelCssClass = (level: number) => {

47810a5b04accd071de1d5d116b7f8df4bc2ba62
ysknsid25 <kengo071225@gmail.com>
2025-01-02 10:34:16 +0900

    fix repack setting
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,9 +2,6 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default {
     plugins: [pluginReact()],
-    html: {
-        template: "./index.html",
-    },
     source: {
         entry: {
             index: "./src/main.tsx",

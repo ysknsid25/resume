a3a69709c3464db0b383dd06fd952dd75d7652b4
ysknsid25 <kengo071225@gmail.com>
2025-01-06 23:43:39 +0900

    add note image
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/components/Tree/TreeImgMap.tsx
+++ src/components/Tree/TreeImgMap.tsx
@@ -1,64 +1,66 @@
-import atu from "../../assets/maid/atu.png"
-import buy from "../../assets/maid/buy.png"
-import cantsee from "../../assets/maid/cantsee.png"
-import cry from "../../assets/maid/cry.png"
-import crycry from "../../assets/maid/crycry.png"
-import devil from "../../assets/maid/devil.png"
-import ehhen from "../../assets/maid/ehhen.png"
-import engel from "../../assets/maid/engel.png"
-import guruguru from "../../assets/maid/guruguru.png"
-import here from "../../assets/maid/here.png"
-import housin from "../../assets/maid/housin.png"
-import inscreen from "../../assets/maid/inscreen.png"
-import kiri from "../../assets/maid/kiri.png"
-import muu from "../../assets/maid/muu.png"
-import nophoto from "../../assets/maid/nophoto.png"
-import paperplane from "../../assets/maid/paperplane.png"
-import pointed from "../../assets/maid/pointed.png"
-import sleep from "../../assets/maid/sleep.png"
-import spana from "../../assets/maid/spana.png"
-import takurami from "../../assets/maid/takurami.png"
-import ummmmm from "../../assets/maid/ummmmm.png"
-import wai from "../../assets/maid/wai.png"
-import work from "../../assets/maid/work.png"
-import zenn from "../../assets/program/logo-only.svg"
-import tora from "../../assets/community/tora.png"
-import nomos from "../../assets/community/nomos.png"
-import fm from "../../assets/community/fm.png"
-import techbookfest from "../../assets/sns/techbookfest.webp"
-import hitocolor from "../../assets/community/hitocolor.png"
-import qiita from "../../assets/sns/qiita.png"
+import atu from "../../assets/maid/atu.png";
+import buy from "../../assets/maid/buy.png";
+import cantsee from "../../assets/maid/cantsee.png";
+import cry from "../../assets/maid/cry.png";
+import crycry from "../../assets/maid/crycry.png";
+import devil from "../../assets/maid/devil.png";
+import ehhen from "../../assets/maid/ehhen.png";
+import engel from "../../assets/maid/engel.png";
+import guruguru from "../../assets/maid/guruguru.png";
+import here from "../../assets/maid/here.png";
+import housin from "../../assets/maid/housin.png";
+import inscreen from "../../assets/maid/inscreen.png";
+import kiri from "../../assets/maid/kiri.png";
+import muu from "../../assets/maid/muu.png";
+import nophoto from "../../assets/maid/nophoto.png";
+import paperplane from "../../assets/maid/paperplane.png";
+import pointed from "../../assets/maid/pointed.png";
+import sleep from "../../assets/maid/sleep.png";
+import spana from "../../assets/maid/spana.png";
+import takurami from "../../assets/maid/takurami.png";
+import ummmmm from "../../assets/maid/ummmmm.png";
+import wai from "../../assets/maid/wai.png";
+import work from "../../assets/maid/work.png";
+import zenn from "../../assets/program/logo-only.svg";
+import tora from "../../assets/community/tora.png";
+import nomos from "../../assets/community/nomos.png";
+import fm from "../../assets/community/fm.png";
+import techbookfest from "../../assets/sns/techbookfest.webp";
+import hitocolor from "../../assets/community/hitocolor.png";
+import qiita from "../../assets/sns/qiita.png";
+import note from "../../assets/sns/note.png";
 
-const treeImgMap = new Map<string, string>
-treeImgMap.set("atu", atu)
-treeImgMap.set("buy", buy)
-treeImgMap.set("cry", cry)
-treeImgMap.set("crycry", crycry)
-treeImgMap.set("cantsee", cantsee)
-treeImgMap.set("devil", devil)
-treeImgMap.set("ehhen", ehhen)
-treeImgMap.set("engel", engel)
-treeImgMap.set("guruguru", guruguru)
-treeImgMap.set("here", here)
-treeImgMap.set("housin", housin)
-treeImgMap.set("inscreen", inscreen)
-treeImgMap.set("kiri", kiri)
-treeImgMap.set("muu", muu)
-treeImgMap.set("nophoto", nophoto)
-treeImgMap.set("paperplane", paperplane)
-treeImgMap.set("pointed", pointed)
-treeImgMap.set("sleep", sleep)
-treeImgMap.set("spana", spana)
-treeImgMap.set("takurami", takurami)
-treeImgMap.set("ummmmm", ummmmm)
-treeImgMap.set("wai", wai)
-treeImgMap.set("work", work)
-treeImgMap.set("zenn", zenn)
-treeImgMap.set("qiita", qiita)
-treeImgMap.set("tora", tora)
-treeImgMap.set("nomos", nomos)
-treeImgMap.set("fm", fm)
-treeImgMap.set("techbookfest", techbookfest)
-treeImgMap.set("hitocolor", hitocolor)
+const treeImgMap = new Map<string, string>();
+treeImgMap.set("atu", atu);
+treeImgMap.set("buy", buy);
+treeImgMap.set("cry", cry);
+treeImgMap.set("crycry", crycry);
+treeImgMap.set("cantsee", cantsee);
+treeImgMap.set("devil", devil);
+treeImgMap.set("ehhen", ehhen);
+treeImgMap.set("engel", engel);
+treeImgMap.set("guruguru", guruguru);
+treeImgMap.set("here", here);
+treeImgMap.set("housin", housin);
+treeImgMap.set("inscreen", inscreen);
+treeImgMap.set("kiri", kiri);
+treeImgMap.set("muu", muu);
+treeImgMap.set("nophoto", nophoto);
+treeImgMap.set("paperplane", paperplane);
+treeImgMap.set("pointed", pointed);
+treeImgMap.set("sleep", sleep);
+treeImgMap.set("spana", spana);
+treeImgMap.set("takurami", takurami);
+treeImgMap.set("ummmmm", ummmmm);
+treeImgMap.set("wai", wai);
+treeImgMap.set("work", work);
+treeImgMap.set("zenn", zenn);
+treeImgMap.set("qiita", qiita);
+treeImgMap.set("tora", tora);
+treeImgMap.set("nomos", nomos);
+treeImgMap.set("fm", fm);
+treeImgMap.set("techbookfest", techbookfest);
+treeImgMap.set("hitocolor", hitocolor);
+treeImgMap.set("note", note);
 
-export default treeImgMap
+export default treeImgMap;

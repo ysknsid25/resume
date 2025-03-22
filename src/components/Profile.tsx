bf23520c8b541372a2a0a98927c04f52b0ad0106
ysknsid25 <kengo071225@gmail.com>
2025-03-23 00:08:24 +0900

    update resume
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,26 @@
 import icon from "../assets/icon.png";
+import twitter from "../assets/sns/twitter.png";
+import amazon from "../assets/sns/amazon.png";
+import github from "../assets/sns/github.png";
+import speakerdeck from "../assets/sns/speakerdeck.png";
+import hatena from "../assets/sns/hatena.png";
+
+type SNS = {
+    url: string;
+    icon: string;
+};
+
+const snsList: SNS[] = [
+    { url: "https://x.com/ysknsid25", icon: twitter },
+    { url: "https://blog.inorinrinrin.com/", icon: hatena },
+    { url: "https://speakerdeck.com/ysknsid25", icon: speakerdeck },
+    { url: "https://github.com/ysknsid25", icon: github },
+    {
+        url: "https://www.amazon.jp/hz/wishlist/ls/969G9ER8XWFW?ref_=wl_share",
+        icon: amazon,
+    },
+];
+
 export const Profile = () => {
     return (
         <div className="mb-4">
@@ -17,8 +39,25 @@ export const Profile = () => {
                     blessing software
                 </p>
                 <p className="mt-4 text-center text-sm text-gray-600">
-                    誰かにずっと覚えていてもらいたい
+                    完全在宅 programmer
                 </p>
+            </div>
+            <div className="flex items-center justify-center mt-8">
+                {snsList.map((sns, index) => (
+                    <div key={index} className="mx-4">
+                        <a
+                            href={sns.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                className="inline-block md:h-12 md:w-12 h-8 w-8"
+                                src={sns.icon}
+                                alt="icon"
+                            />
+                        </a>
+                    </div>
+                ))}
             </div>
         </div>
     );
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,23 +1,17 @@
 import { Profile } from "../components/Profile";
 import { Skills } from "../components/Skills";
-import { Social } from "../components/Social";
-import { Communities } from "../components/Communities";
 import { Certifications } from "../components/Certifications";
 import { Career } from "../components/Career";
 import { Conference } from "../components/Conference";
 import { SideJob } from "../components/SideJob";
 import { Frame } from "../components/Frame";
 import { GitHubContributionsGraph } from "../components/GitHubContributionsGraph";
-import { PopularAllArticle } from "../components/PopularAllArticles";
 
 export const Top = () => {
     return (
         <div className="grid grid-cols-1 gap-4 w-full mt-4 mb-8">
             <Frame>
                 <Profile></Profile>
-            </Frame>
-            <Frame>
-                <Social></Social>
             </Frame>
             <Frame>
                 <GitHubContributionsGraph></GitHubContributionsGraph>
@@ -36,9 +30,6 @@ export const Top = () => {
             </Frame>
             <Frame>
                 <SideJob></SideJob>
-            </Frame>
-            <Frame>
-                <Communities></Communities>
             </Frame>
         </div>
     );

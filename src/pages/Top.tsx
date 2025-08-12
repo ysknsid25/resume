d9ee03c399286a02ea660e2c52bb115c24069da4
ysknsid25 <kengo071225@gmail.com>
2025-08-12 14:24:37 +0900

    delete page
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,36 +1,10 @@
-import { Profile } from "../components/Profile";
-import { Skills } from "../components/Skills";
-import { Certifications } from "../components/Certifications";
-import { Career } from "../components/Career";
-import { Conference } from "../components/Conference";
-import { SideJob } from "../components/SideJob";
-import { Frame } from "../components/Frame";
-import { GitHubContributionsGraph } from "../components/GitHubContributionsGraph";
-
 export const Top = () => {
     return (
         <div className="grid grid-cols-1 gap-4 w-full mt-4 mb-8">
-            <Frame>
-                <Profile></Profile>
-            </Frame>
-            <Frame>
-                <GitHubContributionsGraph></GitHubContributionsGraph>
-            </Frame>
-            <Frame>
-                <Skills></Skills>
-            </Frame>
-            <Frame>
-                <Certifications></Certifications>
-            </Frame>
-            <Frame>
-                <Conference></Conference>
-            </Frame>
-            <Frame>
-                <Career></Career>
-            </Frame>
-            <Frame>
-                <SideJob></SideJob>
-            </Frame>
+            <p>
+                this page has moved permanently. check{" "}
+                <a href="https://github.com/ysknsid25">GitHub Profile</a>
+            </p>
         </div>
     );
 };

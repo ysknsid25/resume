722d49dc6adb2c60d1a6e119c88e33ccad8e15cf
ysknsid25 <kengo071225@gmail.com>
2025-11-21 06:03:16 +0900

    Make Articles & GitHub Contributions Data

--- src/data/HatenaBookmarkData.ts
+++ src/data/HatenaBookmarkData.ts
@@ -32,7 +32,7 @@ export const HatenaBookmarkData = [
   },
   {
     "year": "2025",
-    "sum": 12775,
+    "sum": 12774,
     "bookmarkRanking": [
       {
         "title": "定期的に何度も読み返している個人的名エントリを置いておく",
